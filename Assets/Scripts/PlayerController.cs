6c4861142b5ec7fdf6a3b50e89c7d147261cb447
Frédéric Darré <frederic.darre@outlook.com>
2022-05-22 16:42:18 +0200

    Handle character controls and movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = new Vector2(_inputX * moveSpeed, _rigidbody.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _inputX = context.ReadValue<Vector2>().x;
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+        //if is grounded;
+        if (context.performed)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+        }
+    }
+
+    private float _inputX;
+    private Rigidbody2D _rigidbody;
+}
