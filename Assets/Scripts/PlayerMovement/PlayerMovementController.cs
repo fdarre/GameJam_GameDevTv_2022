a2e24c422593991453db51f9af4f3b852fd1623f
Frédéric Darré <frederic.darre@outlook.com>
2022-05-23 16:34:46 +0200

    Handle player animations

--- Assets/Scripts/PlayerMovement/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovementController.cs
@@ -8,10 +8,20 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
 
+    public enum MovementState
+    {
+        Idle,
+        Running,
+        Jumping,
+        Falling
+    }
+    
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _coll = GetComponent<Collider2D>();
+        _animator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         _groundMask = LayerMask.GetMask("Ground");
     }
 
@@ -19,6 +29,42 @@ public class PlayerMovementController : MonoBehaviour
     {
         _isGrounded = CheckIsGrounded();
         _rigidbody.velocity = new Vector2(_inputX * moveSpeed, _rigidbody.velocity.y);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        MovementState state;
+
+        if (_rigidbody.velocity.x > 0.1f)
+        {
+            state = MovementState.Running;
+            _spriteRenderer.flipX = false;
+        }
+        else if (_rigidbody.velocity.x < -0.1f)
+        {
+            state = MovementState.Running;
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            state = MovementState.Idle;
+        }
+        
+        if (_rigidbody.velocity.y > 0.1f)
+        {
+            state = MovementState.Jumping;
+        }
+        else if (_rigidbody.velocity.y < -0.1f)
+        {
+            state = MovementState.Falling;
+        }
+
+        if (_currentState != state)
+        {
+            _currentState = state;
+            _animator.SetInteger(_stateParameter, (int) state);
+        }
     }
 
     private bool CheckIsGrounded()
@@ -47,4 +93,9 @@ public class PlayerMovementController : MonoBehaviour
     private int _groundMask;
     private RaycastHit2D _hit;
     private Collider2D _coll;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private MovementState _currentState = MovementState.Idle;
+
+    private static readonly int _stateParameter = Animator.StringToHash("state");
 }
