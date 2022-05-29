d3fdeca80da79c6aea23ec1fb1b76c3e0eee9f24
Frédéric Darré <frederic.darre@outlook.com>
2022-05-29 16:03:42 +0200

    Fix bug where player fall through tilemap colliders

--- Assets/Scripts/Collisions/CollisionDamage.cs
+++ Assets/Scripts/Collisions/CollisionDamage.cs
@@ -5,30 +5,15 @@ namespace Collisions
 {
     public class CollisionDamage : MonoBehaviour
     {
-        #region Init
-
-        private void Start()
-        {
-            _playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
-        }
-
-        #endregion
-
         #region Collisions
 
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.CompareTag("Player"))
             {
-                _playerHealth.GetHit();
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>().GetHit();
             }
         }
-
-        #endregion
-        
-        #region Private Variables
-
-        private PlayerHealth _playerHealth;
 
         #endregion
     }
